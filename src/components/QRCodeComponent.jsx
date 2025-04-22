eaf1d580e1ca21dae445339b74b6bcc73e7af73b
Данияр Нурмагамбетов <daniyar@MacBook-Air-Daniar.local>
2025-04-22 16:49:48 +0500

    72

--- src/components/QRCodeComponent.jsx
+++ src/components/QRCodeComponent.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import QRCode from "qrcode.react"
-import { useState } from "react";
+import React, { useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 
 const QRCodeComponent = () => {
-    const currentUrl = window.location.href
-    const [isVisible, setIsVisible] = useState(false); // Состояние для управления видимостью
-      
+    const currentUrl = window.location.href;
+    const [isVisible, setIsVisible] = useState(false);
+
     const toggleVisibility = () => {
-        setIsVisible(!isVisible); // Переключаем видимость
+        setIsVisible(!isVisible);
         console.log("переключено");
-        
     };
 
-    return(
+    return (
         <div>
-            <button onClick={toggleVisibility}> {isVisible ? 'Скрыть QR-код' : 'Показать QR-код'}</button>
+            <button onClick={toggleVisibility}>
+                {isVisible ? 'Скрыть QR-код' : 'Показать QR-код'}
+            </button>
             {isVisible && (
                 <div>
                     <h1>Сканируй</h1>
-                    <QRCode value={currentUrl} size={256}/>
+                    <QRCodeCanvas value={currentUrl} size={256} />
                 </div>
             )}
         </div>
-    )
-}
-export default QRCodeComponent
+    );
+};
+
+export default QRCodeComponent;
