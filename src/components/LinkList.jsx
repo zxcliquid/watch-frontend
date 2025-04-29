c15798b22793bda86c571f15ee3ec455623ba08b
Данияр Нурмагамбетов <daniyar@MacBook-Air-Daniar.local>
2025-04-29 23:11:39 +0500

    110

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -3,11 +3,6 @@ import React from "react";
 const LinkList = ({ isOpen, onClose }) => {
     if (!isOpen) return null
 
-    const fetchPopularVideos = async () => {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=US&key=AIzaSyCVmKha63Eqiq3Fuhq3SdTffLCZYMnYY3U`);
-        const data = await response.json();
-        return data.items; // возвращает массив популярных видео
-    };
 
     return (
         <div className="modal-overlay">
@@ -15,13 +10,9 @@ const LinkList = ({ isOpen, onClose }) => {
                 <button className="modal-close-btn" onClick={onClose}>×</button>
                 <h2>Список ссылок</h2>
                     <ul>
-                        {videos.map(video => (
-                        <li key={video.id}>
-                            <a href={`https://www.youtube.com/watch?v=${video.id}`} target="_blank" rel="noopener noreferrer">
-                            {video.snippet.title}
-                            </a>
-                                </li>
-                            ))}
+                        <li><a href="https://example1.com" target="_blank" rel="noopener noreferrer">Ссылка 1</a></li>
+                        <li><a href="https://example2.com" target="_blank" rel="noopener noreferrer">Ссылка 2</a></li>
+                        <li><a href="https://example3.com" target="_blank" rel="noopener noreferrer">Ссылка 3</a></li>
                     </ul>
             </div>
         </div>
