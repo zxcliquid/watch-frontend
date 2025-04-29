e41763398814ce00eeadf187ed5294925e861ddb
Данияр Нурмагамбетов <daniyar@MacBook-Air-Daniar.local>
2025-04-29 22:06:20 +0500

    102

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const LinkList = ({ isOpen, onClose }) => {
     if (!isOpen) return null
-}
 
-return (
-    <div className="modal-overlay">
-        <div className="modal-content">
-            <button className="modal-close-btn" onClick={onClose}>×</button>
-            <h2>Список ссылок</h2>
-                <ul>
-                    <li><a href="https://example1.com" target="_blank" rel="noopener noreferrer">Ссылка 1</a></li>
-                    <li><a href="https://example2.com" target="_blank" rel="noopener noreferrer">Ссылка 2</a></li>
-                    <li><a href="https://example3.com" target="_blank" rel="noopener noreferrer">Ссылка 3</a></li>
-                </ul>
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button className="modal-close-btn" onClick={onClose}>×</button>
+                <h2>Список ссылок</h2>
+                    <ul>
+                        <li><a href="https://example1.com" target="_blank" rel="noopener noreferrer">Ссылка 1</a></li>
+                        <li><a href="https://example2.com" target="_blank" rel="noopener noreferrer">Ссылка 2</a></li>
+                        <li><a href="https://example3.com" target="_blank" rel="noopener noreferrer">Ссылка 3</a></li>
+                    </ul>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default LinkList
